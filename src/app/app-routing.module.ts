6e573c60c290c435c88931526f5ba6dd1d577b00
Moh4medNajjar <najjarmohamed443@gmail.com>
2023-07-08 19:28:53 +0100

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
+import { InfoComponent } from './info/info.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DemandeComponent } from './demande/demande.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'demande', component: DemandeComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', redirectTo: '/demande' }
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
